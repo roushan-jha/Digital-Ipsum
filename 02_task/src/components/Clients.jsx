3a7de4f8f64ed53c65bd0d185e606bb590b375bd
roushan-jha <jharoushan20@gmail.com>
2024-06-17 14:53:48 +0530

    changed border of Clients component

--- 02_task/src/components/Clients.jsx
+++ 02_task/src/components/Clients.jsx
@@ -5,7 +5,9 @@ import starsup from '../assets/starsup.png'
 
 const Clients = () => {
   return (
-    <div className='w-full flex justify-between items-center px-20 py-10 border-y-2 border-orange-500 from-orange-500 to-transparent'>
+    <div>
+      <div className='fade h-[2px] bg-gradient-to-r from-transparent via-orange-400'></div>
+      <div className='w-full flex justify-between items-center px-20 py-10'>
         <p className='w-[50%] font-satoshi font-normal text-lg'>Our Clients:</p>
         <div className='flex justify-between w-[50%]'>
             <img src={marketSavy} alt="Company Logo" className='opacity-50' />
@@ -13,6 +15,8 @@ const Clients = () => {
             <img src={overlays} alt="Company Logo" className='opacity-50' />
             <img src={starsup} alt="Company Logo" className='opacity-50' />
         </div>
+      </div>
+      <div className='fade h-[2px] bg-gradient-to-r from-transparent via-orange-400'></div>
     </div>
   )
 }
